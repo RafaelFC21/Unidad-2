a6ce2ee4fd656e2f9b99a44c7fab2c861ef6e0d2
Rafael Franco <43421180+RafaelFC21@users.noreply.github.com>
2018-10-01 22:41:09 -0700

    E2-2
    Unidad 2 Ejercicio 2

---
+++ E2-2 Franco Corona Rafael/EJERCICIO 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2_2_Franco_Corona_Rafael
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeticiones, suma = 0;
+            Console.Write("¿Cuantos numeros desea desplegar?: ");
+            repeticiones = int.Parse(Console.ReadLine()); 
+            Fibonacci serie = new Fibonacci(repeticiones, suma);
+            Console.WriteLine("Fibonacci recursivo: ");
+            Stopwatch tiempo1 = Stopwatch.StartNew();
+            serie.recursivo(1, 0, repeticiones);
+            Console.WriteLine("Tiempo: " + tiempo1.Elapsed.TotalMilliseconds + " milisegundos");
+            Console.WriteLine("Fibonacci for: ");
+            Stopwatch tiempo2 = Stopwatch.StartNew();
+            serie.iteractivo();
+            Console.WriteLine("Tiempo: " + tiempo2.Elapsed.TotalMilliseconds + " milisegundos");
+            Console.WriteLine("Presione <enter> para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ E2-2 Franco Corona Rafael/EJERCICIO 1/fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_2_Franco_Corona_Rafael
+{
+    class Fibonacci
+    {
+        int repeticiones, suma;
+        public Fibonacci(int repeticiones, int suma)
+        {
+            this.suma = suma;
+            this.repeticiones = repeticiones;
+        }
+        public void recursivo(double inicial, double final, int repeticiones)
+        {
+            double suma;
+            suma = inicial + final;
+            final = inicial;
+            inicial = suma;
+            if (repeticiones > 0)
+            {
+                Console.WriteLine(suma);
+                recursivo(inicial, final, repeticiones - 1);
+            }
+        }
+        public void iteractivo()
+        {
+            int inicial = 1;
+            int final = 0;
+            for (int contador = 0; contador < repeticiones; contador++)
+            {
+                suma = inicial + final;
+                final = inicial;
+                inicial = suma;
+                Console.WriteLine(suma);
+            }
+        }
+    }
+}
